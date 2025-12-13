4e2820f9e7d8e914dccb304f65fe6c56e32946e0
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-12-13 13:42:58 +0100

    feat: update render method to improve template handling and remove redundant code

--- src/Controllers/AppController.php
+++ src/Controllers/AppController.php
@@ -13,22 +13,17 @@ class AppController
         return $_SERVER["REQUEST_METHOD"] === 'POST';
     }
 
-    protected function render(string $template = null, array $variables = [])
+    protected function render(?string $template = null, array $variables = []): void
     {
         $templatePath = 'public/views/' . $template . '.html';
         $output = 'public/views/404.html';
 
         if (file_exists($templatePath)) {
             extract($variables);
-
-            ob_start();
-            include $templatePath;
-            $output = ob_get_clean();
-        } else {
-            ob_start();
-            include $templatePath;
-            $output = ob_get_clean();
         }
+        ob_start();
+        include $templatePath;
+        $output = ob_get_clean();
         echo $output;
     }
 }
