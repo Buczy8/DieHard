1beb9b0882b86c6ec0e8bcf8d6a98e0fbeaf38fd
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2025-12-14 22:19:27 +0100

    feat: Enhance basic login and registration with error messages and password confirmation

--- src/Controllers/SecurityController.php
+++ src/Controllers/SecurityController.php
@@ -1,15 +1,102 @@
 <?php
 
 namespace App\Controllers;
+
+use App\Database;
+use App\DTO\CreateUserDTO;
+use App\Models\User;
+use App\Repository\UserRepository;
+
 class SecurityController extends AppController
 {
+    private $userRepository;
+
+    public function __construct()
+    {
+        $db = Database::getInstance();
+        $this->userRepository = new UserRepository($db);
+    }
+
     public function login()
     {
-        return $this->render('login');
+        if ($this->isGet()) {
+            return $this->render("login");
+        }
+
+        $email = $_POST["email"] ?? '';
+        $password = $_POST["password"] ?? '';
+
+        if (empty($email) || empty($password)) {
+            return $this->render("login", ["message" => "Fill all fields"]);
+        }
+
+        $user = $this->userRepository->getUserByEmail($email);
+
+        if (!$user) {
+            return $this->render("login", ["message" => "User not found"]);
+        }
+        if (!password_verify($password, $user->password)) {
+            return $this->render("login", ["message" => "Wrong password"]);
+        }
+
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/dicegame");
     }
 
     public function register()
     {
-        return $this->render('register');
+        if ($this->isGet()) {
+            return $this->render("register");
+        }
+
+        $formData = [
+            'email' => $_POST["email"] ?? '',
+            'password' => $_POST["password"] ?? '',
+            'username' => trim($_POST["user-name"] ?? ''),
+            'role' => 'user'
+        ];
+        $passwordConfirmation = $_POST["passwordConfirmation"] ?? '';
+
+        $messages = [];
+
+        if (empty($formData['email']) || empty($formData['password']) || empty($formData['username']) || empty($passwordConfirmation)) {
+            return $this->render("register", ["message" => "nie wszytkie pola są wypiełnoine"]);
+        }
+
+        if ($formData['password'] !== $passwordConfirmation) {
+            return $this->render("register", ["message" => "hasła się nie zgdzaja"]);
+        }
+
+
+        try {
+            $userDTO = CreateUserDTO::fromRequest($formData);
+            $existingUser = $this->userRepository->getUserByEmail($userDTO->email);
+
+            if ($existingUser) {
+                return $this->render("register", ["message" => "Użytkownik o tym adresie email już istnieje!"]);
+            }
+
+            $hashedPassword = password_hash($userDTO->password, PASSWORD_DEFAULT);
+
+            $user = new User(
+                email: $userDTO->email,
+                username: $userDTO->username,
+                password: $hashedPassword,
+                role: $userDTO->role
+            );
+
+            $this->userRepository->createUser($user);
+
+            return $this->render("login", ["message" => "Zarejestrowano poprawnie! Zaloguj się."]);
+
+        } catch (\Exception $e) {
+            return $this->render('register', [
+                'messages' => ['Wystąpił błąd serwera. Spróbuj ponownie później.'],
+                'old_values' => ['email' => $formData['email'], 'nickname' => $formData['username']]
+            ]);
+        }
     }
 }
