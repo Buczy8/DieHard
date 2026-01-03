cc1485c2a90c578ec180130db5a2110442be3211
Paweł Buczek <151944237+Buczy8@users.noreply.github.com>
2026-01-03 17:19:36 +0100

    feat: Enhance dashboard with dynamic user statistics and personalized greeting

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -1,12 +1,40 @@
 <?php
 namespace App\Controllers;
 use App\Annotation\RequireLogin;
-use App\Controllers\AppController;
+use App\DTO\UserStatisticsResponseDTO;
+use App\Repository\UserRepository;
+use App\Repository\UserStatisticsRepository;
 
 class DashboardController extends AppController {
+
+    private UserStatisticsRepository $statsRepository;
+    private $userRepository;
+
+    public function __construct()
+    {
+
+        $this->statsRepository = UserStatisticsRepository::getInstance();
+        $this->userRepository = UserRepository::getInstance();
+    }
+
     #[RequireLogin]
     public function index()
     {
-        $this->render('dashboard');
+        $userId = $_SESSION['user_id'] ?? null;
+        $user = $this->userRepository->getUserById($userId);
+
+        if (!$user) {
+            header('Location: /logout');
+            exit;
+        }
+
+        $statsModel = $this->statsRepository->getStatsByUserEmail($user->email);
+
+        $statsDTO = UserStatisticsResponseDTO::fromEntity($statsModel);
+
+        $this->render('dashboard', [
+            'username' => $user->username,
+            'stats' => $statsDTO,
+        ]);
     }
 }
